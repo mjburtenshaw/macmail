c1d55977a9e9c56a01920a80f54bb969c3855b61
Malcolm Burtenshaw <mjburtenshaw@gmail.com>
2023-09-16 17:24:20 -0600

    build: version 1.1.4

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,18 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.macmail = void 0;
-const letter_1 = require("./letter");
-const mail_1 = require("./mail");
 const useConfig_1 = require("./useConfig");
-(0, useConfig_1.useConfig)();
-exports.macmail = {
-    letter: letter_1.letter,
-    mail: mail_1.mail,
-};
-exports.default = exports.macmail;
+let letter;
+let mail;
+async function initialize() {
+    await (0, useConfig_1.useConfig)();
+    const letterModule = require('./letter');
+    const mailModule = require('./mail');
+    letter = letterModule.letter;
+    mail = mailModule.mail;
+}
+initialize().then(() => {
+    module.exports = {
+        letter,
+        mail,
+    };
+});
